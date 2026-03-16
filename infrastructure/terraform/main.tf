a0c8a99f9ac76583a893b8fea9ac76dd32e1dbc7
root <root@Ulan.>
2026-03-16 15:32:32 +0500

    12th commit

---
+++ infrastructure/terraform/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "DevOps-Example"
+  }
+}
